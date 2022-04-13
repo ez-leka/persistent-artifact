07ed6ea8b1e4b9f67a9ec8cc2761f5cf3dafa8fe
Elana Zobin <leka@gnode.org>
2022-04-12 17:13:41 -0700

    --all

---
+++ index.js
@@ -0,0 +1,23 @@
+github = require('@actions/github');
+core = require('@actions/core');
+
+const test = async () => {
+    const github_token = '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';
+    const workflow = 'test-on-demand.yml';
+    const artifact_name = 'my-runable';
+
+    const client = github.getOctokit(github_token);
+
+    const params = {
+        owner: 'ez_leka',
+        repo: 'test-on-demand-action-runner',
+        workflow_id: workflow,        
+    }
+    const workflows = await client.paginate('GET /repos/{owner}/{repo}/actions/runs', params);
+
+    core.info(JSON.stringify(workflows))
+    core.info('Done');
+    
+}
+
+test();
